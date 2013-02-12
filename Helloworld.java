3ab192d63557137a79b55ca4554643a8dcab4368
Mitchell Foley <mitch.foley@mail.com>
2013-02-12 16:49:44 -0500

    Started learning Java

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+// The classic Hello World program
+class HelloWorld {
+
+	public static void main(String[] args) {
+
+		//Writes hello world in the terminal
+		System.out.println("Hello World!");
+
+	}
+}
--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ Mitchell Foley
 Feb 12 2013
 Simple Game to help me learn java
 
+compiler path: C:\Program Files (x86)\Java\jdk1.6.0_14\bin
+path: C:\Users\foley family 1\Documents\GitHub\jGame
